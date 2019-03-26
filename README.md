6a582d6271f5baaffc7f5476fe7cf3de8000bb6f
Elliot Forbes <emforce@live.com>
2019-03-26 21:58:43 +0000

    Added code from tutorial

---
+++ README.md
@@ -0,0 +1,6 @@
+Go Functions Tutorial
+=======================
+
+![Travis-CI Status](https://travis-ci.org/TutorialEdge/go-functions-tutorial.svg?branch=master)
+
+TutorialEdge Link : [Go Functions Tutorial](https://tutorialedge.net/golang/go-functions-tutorial/)
--- main.go
+++ main.go
@@ -1,7 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
+
+// myFunction is a simple function that takes in two strings
+// concatenates them into one string and returns the result
+func myFunction(firstName string, lastName string) string {
+	return firstName + " " + lastName
+}
+
+// myFunctionWithErr takes in the same two strings as before
+// but checks to see if the first name is greater than 1 character
+// in length. If it isn't it returns an error
+func myFunctionWithErr(firstName string, lastName string) (string, error) {
+	if len(firstName) <= 1 {
+		return "", errors.New("First Name Must Be At Least 2 letters long")
+	}
+
+	// if the first name passes the check, we return the
+	// concatenated names and a nil error value
+	return firstName + " " + lastName, nil
+}
+
+// addOne is a function that returns
+// a function which in turn returns an int value
+func addOne() func() int {
+	var x int
+	return func() int {
+		x++
+		return x + 1
+	}
+}
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Go Functions Tutorial")
+
+	// Test our first simple function which just returns
+	// one value which is our concatenated name
+	fullName := myFunction("Elliot", "Forbes")
+	fmt.Println(fullName)
+
+	// Test our second function which returns 2 values,
+	// our name and an error if there is any
+	fullName2, err := myFunctionWithErr("Elliot", "Forbes")
+	if err != nil {
+		fmt.Println("We Handle the error if there is one")
+	}
+	fmt.Println(fullName2)
+
+	// Let's test our our function that returns a
+	// function which returns an int.
+	myFunc := addOne()
+	fmt.Println(myFunc()) // prints out 2
+	fmt.Println(myFunc()) // prints out 3
+	fmt.Println(myFunc()) // prints out 4
 }
